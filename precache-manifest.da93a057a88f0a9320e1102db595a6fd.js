e8c7387ead183cd2a8fa402b9634c5fb90923d02
kevincpan <kcpan@ucsd.edu>
2020-04-13 18:06:12 -0700

    Updates

---
+++ precache-manifest.da93a057a88f0a9320e1102db595a6fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ba5a8ca1d951365ff819316e627e64",
+    "url": "/verta/index.html"
+  },
+  {
+    "revision": "ee14d5f6d8a2318770dd",
+    "url": "/verta/static/css/main.2d93bd4e.chunk.css"
+  },
+  {
+    "revision": "49f4293d83423ea6fba5",
+    "url": "/verta/static/js/2.24c63335.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/verta/static/js/2.24c63335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee14d5f6d8a2318770dd",
+    "url": "/verta/static/js/main.1700fe3e.chunk.js"
+  },
+  {
+    "revision": "2263a8cfeca5971dbe8d",
+    "url": "/verta/static/js/runtime-main.b392d7ad.js"
+  }
+]);
